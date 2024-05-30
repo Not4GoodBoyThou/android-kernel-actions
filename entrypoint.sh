5e193234f835339d0c94a0ea908868189ad43691
Not4GoodBoyThou <78427384+Not4GoodBoyThou@users.noreply.github.com>
2024-05-30 19:02:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -201,9 +201,11 @@ start_time="$(date +%s)"
 date="$(date +%d%m%Y-%I%M)"
 tag="$(git branch | sed 's/*\ //g')"
 echo "branch/tag: $tag"
-echo "make options:" $arch_opts $make_opts $host_make_opts
+echo "make options:" $arch_opts $make_opts $host_make_opts $defconfig
 msg "Generating defconfig from \`make $defconfig\`..."
-if ! make O=out $arch_opts $make_opts $host_make_opts "$defconfig"; then
+echo $PWD
+echo "$(ls $PWD)"
+if ! make O=out $arch_opts $make_opts $host_make_opts $defconfig; then
     err "Failed generating .config, make sure it is actually available in arch/${arch}/configs/ and is a valid defconfig file"
     exit 2
 fi
